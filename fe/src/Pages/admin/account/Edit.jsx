4a2f7be01af88712b9be0744bdb1c23aff2ee47f
AdilEffendiii <saadilaeffendi@gmail.com>
2024-05-24 01:51:59 +0800

    Refactor Frontend File

--- fe/src/Pages/admin/account/Edit.jsx
+++ fe/src/Pages/admin/account/Edit.jsx
@@ -1,9 +1,189 @@
-import Edituser from "../../../Components/Edituser";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useLocation, Link } from "react-router-dom";
+import axios from "axios";
 
 export function AdminAccountEdit() {
+  const location = useLocation();
+  const { userId } = location.state;
+
+  const [formData, setFormData] = useState({
+    name: "",
+    role: "",
+    password: "",
+  });
+
+  const [originalPassword, setOriginalPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("token");
+
+    axios
+      .get(`http://127.0.0.1:8000/api/users/${userId}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((response) => {
+        if (response.data.success) {
+          const user = response.data.data;
+          setFormData({
+            name: user.name,
+            role: user.type === 1 ? "operator" : user.type === 3 ? "admin" : "",
+            // password: "",
+          });
+          // setOriginalPassword(user.password); // password originalnya
+        } else {
+          console.error("Gagal mengambil data pengguna", response.data);
+          setError("Failed to fetch user");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        console.error("Error Response:", error.response);
+        setError("Failed to fetch user data");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [userId]);
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData({
+      ...formData,
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.role) {
+      setError("You must choose the role");
+      return;
+    }
+
+    const accessToken = localStorage.getItem("token");
+
+    try {
+      const userType =
+        formData.role === "operator"
+          ? 1
+          : formData.role === "admin"
+          ? 3
+          : undefined;
+
+      const payload = {
+        name: formData.name,
+        type: userType,
+        // password: formData.password || originalPassword, // original password tidak berubah
+      };
+
+      const response = await axios.put(
+        `http://127.0.0.1:8000/api/users/${userId}`,
+        payload,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        console.log("User updated successfully");
+        navigate("/admin/account");
+      } else {
+        console.error("Failed to update user");
+        setError("Nothing Change");
+      }
+    } catch (error) {
+      console.error("Nothing Change:", error);
+      if (error.response && error.response.status === 422) {
+        setError(
+          "Error: " + (error.response.data.message || "Unprocessable Entity")
+        );
+      } else {
+        setError("Nothing Change");
+      }
+    }
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <>
-      <Edituser />
-    </>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <form
+        className="bg-white p-8 rounded-3xl shadow-lg w-2/5"
+        onSubmit={handleSubmit}
+      >
+        <h2 className="text-2xl font-bold mb-6 text-center">Edit Account</h2>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="name"
+          >
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Name"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="flex space-x-4 mb-4">
+          <label className="inline-flex items-center">
+            <input
+              type="radio"
+              name="role"
+              value="operator"
+              checked={formData.role === "operator"}
+              onChange={handleChange}
+              className="form-radio text-blue-600"
+            />
+            <span className="ml-2 text-gray-700">Operator</span>
+          </label>
+          <label className="inline-flex items-center">
+            <input
+              type="radio"
+              name="role"
+              value="admin"
+              checked={formData.role === "admin"}
+              onChange={handleChange}
+              className="form-radio text-blue-600"
+            />
+            <span className="ml-2 text-gray-700">Admin</span>
+          </label>
+        </div>
+
+        <Link
+          to="/admin/account/change-password"
+          state={{ userId: userId }}
+          className="text-blue-400 hover:text-blue-700"
+        >
+          Change Password
+        </Link>
+
+        {error && <div className="text-red-500 text-center mb-4">{error}</div>}
+
+        <div className="text-center mt-10">
+          <button
+            type="submit"
+            className="bg-[#697077] text-white shadow hover:bg-[#f8dbb3] font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Save
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
--- fe/src/Pages/operator/turret/Edit.jsx
+++ fe/src/Pages/operator/turret/Edit.jsx
@@ -1,8 +1,192 @@
-import Editturret from "../../../Components/Editturret";
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+
 export function OperatorTurretEdit() {
+  const [formData, setFormData] = useState({
+    path: null,
+    description: "",
+    location: "",
+    secretKey: "",
+  });
+
+  const navigate = useNavigate();
+  const { id_turret } = useParams(); // Menggunakan id_turret sebagai ID
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = localStorage.getItem("token");
+      try {
+        const response = await axios.get(
+          `http://localhost:8000/api/turrets/${id_turret}`, // Menggunakan id_turret sebagai ID dalam URL
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const data = response.data;
+
+        if (data.success) {
+          // Mengatur nilai form berdasarkan data yang diterima dari API
+          setFormData({
+            path: data.data.path,
+            description: data.data.description,
+            location: data.data.location,
+            secretKey: data.data.secretKey,
+          });
+        } else {
+          console.error("Failed to get data for editing turret");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
+    fetchData();
+  }, [id_turret]); // Menggunakan id_turret sebagai dependency agar useEffect dipanggil setiap kali id_turret berubah
+
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+    if (name === "path") {
+      setFormData({
+        ...formData,
+        path: files[0],
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const token = localStorage.getItem("token");
+
+    const formDataToSend = new FormData();
+    formDataToSend.append("path", formData.path);
+    formDataToSend.append("description", formData.description);
+    formDataToSend.append("location", formData.location);
+    formDataToSend.append("secret_key", formData.secretKey);
+
+    try {
+      const response = await axios.put(
+        `http://localhost:8000/api/turrets/${id_turret}`, // Menggunakan id_turret sebagai ID dalam URL
+        formDataToSend,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        console.log("Success to update turret");
+        navigate("/operator/turret");
+      } else {
+        console.error("Failed to update turret");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
-    <>
-      <Editturret />
-    </>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <form
+        className="bg-white p-8 rounded-3xl shadow-lg w-2/5"
+        onSubmit={handleSubmit}
+      >
+        <h2 className="text-2xl font-bold mb-6 text-center">Edit Turret</h2>
+
+        <div className="mb-4 flex justify-center">
+          <div className="w-96">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="path"
+            >
+              Turret Image (Path)
+            </label>
+            <input
+              type="file"
+              name="path"
+              accept="image/*"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        <div className="mb-4 flex justify-center mt-5">
+          <div className="w-96">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="description"
+            >
+              Description
+            </label>
+            <input
+              type="text"
+              name="description"
+              placeholder="Enter Description"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              value={formData.description}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        <div className="mb-4 flex justify-center mt-5">
+          <div className="w-96">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="secretKey"
+            >
+              Secret Key
+            </label>
+            <input
+              type="text"
+              name="secretKey"
+              placeholder="Enter Secret Key"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              value={formData.secretKey}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        <div className="mb-4 flex justify-center mt-5">
+          <div className="w-96">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="location"
+            >
+              Location
+            </label>
+            <input
+              type="text"
+              name="location"
+              placeholder="Enter Location"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              value={formData.location}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        <div className="text-center mt-10">
+          <button
+            type="submit"
+            className="bg-[#697077] text-white shadow hover:bg-[#f8dbb3] font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline "
+          >
+            Update
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }

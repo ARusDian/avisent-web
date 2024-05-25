526e6a7ed50f463a89b4b8c90a0dfa0ead577a97
RUVNE <dian.galleon@gmail.com>
2024-05-26 01:09:57 +0800

    fix: redirect fe route

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,4 +1,10 @@
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import {
+	BrowserRouter,
+	Routes,
+	Route,
+	Outlet,
+	Navigate,
+} from "react-router-dom";
 import Login from "./Pages/Login";
 import Index from "./Pages/Index";
 import { OperatorHome } from "./Pages/operator/home/Index";
@@ -20,50 +26,69 @@ import PrivateRoute from "./Components/PrivateRoute";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Route Public */}
-        <Route index={true} path="/login" element={<Login />} />
+		<BrowserRouter>
+			<Routes>
+				{/* Route Public */}
+				<Route path="/" element={<Navigate to="/login" />} />
+				<Route index={true} path="/login" element={<Login />} />
 
-        {/* Route Admin */}
-        <Route
-          path="admin"
-          element={
-            <PrivateRoute element={() => <AdminLayout />}>
-              <Outlet />
-            </PrivateRoute>
-          }
-        >
-          <Route path="home" element={<AdminHome />} />
-          <Route path="log" element={<AdminLog />} />
-          <Route path="account" element={<AdminAccount />} />
-          <Route path="specification" element={<SpecificationHome />} />
-          <Route path="account/create" element={<AdminAccountCreate />} />
-          <Route path="account/:id/edit" element={<AdminAccountEdit />} />
-          <Route
-            path="account/:id/change-password"
-            element={<AdminAccountChangePassword />}
-          />
-        </Route>
+				{/* Route Admin */}
+				<Route
+					path="admin"
+					element={
+						<PrivateRoute element={() => <AdminLayout />}>
+							<Outlet />
+						</PrivateRoute>
+					}
+				>
+					<Route path="home" element={<AdminHome />} />
+					<Route path="log" element={<AdminLog />} />
+					<Route path="account" element={<AdminAccount />} />
+					<Route
+						path="specification"
+						element={<SpecificationHome />}
+					/>
+					<Route
+						path="account/create"
+						element={<AdminAccountCreate />}
+					/>
+					<Route
+						path="account/:id/edit"
+						element={<AdminAccountEdit />}
+					/>
+					<Route
+						path="account/:id/change-password"
+						element={<AdminAccountChangePassword />}
+					/>
+				</Route>
 
-        {/* Route Operator */}
-        <Route
-          path="operator"
-          element={
-            <PrivateRoute element={() => <OperatorLayout />}>
-              <Outlet />
-            </PrivateRoute>
-          }
-        >
-          <Route path="home" element={<OperatorHome />} />
-          <Route path="log" element={<OperatorLog />} />
-          <Route path="turret" element={<OperatorTurret />} />
-          <Route path="turret/create" element={<OperatorTurretCreate />} />
-          <Route path="turret/:id/edit" element={<OperatorTurretEdit />} />
-          <Route path="turret/:id/control" element={<OperatorTurretControl />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+				{/* Route Operator */}
+				<Route
+					path="operator"
+					element={
+						<PrivateRoute element={() => <OperatorLayout />}>
+							<Outlet />
+						</PrivateRoute>
+					}
+				>
+					<Route path="home" element={<OperatorHome />} />
+					<Route path="log" element={<OperatorLog />} />
+					<Route path="turret" element={<OperatorTurret />} />
+					<Route
+						path="turret/create"
+						element={<OperatorTurretCreate />}
+					/>
+					<Route
+						path="turret/:id/edit"
+						element={<OperatorTurretEdit />}
+					/>
+					<Route
+						path="turret/:id/control"
+						element={<OperatorTurretControl />}
+					/>
+				</Route>
+			</Routes>
+		</BrowserRouter>
   );
 };
 

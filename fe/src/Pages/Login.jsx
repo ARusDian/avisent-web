591cfc9224c01774f397ffbb904b523aac7284ae
AdilEffendiii <saadilaeffendi@gmail.com>
2024-05-28 07:17:55 +0800

    fix fe (all)

--- fe/src/Pages/Login.jsx
+++ fe/src/Pages/Login.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import COVER_IMAGE from "../assets/turetgun.jpg";
+import BACKGROUND_IMAGE from "../assets/bg2.png";
 
 const Login = () => {
   const [values, setValues] = useState({ name: "", password: "" });
@@ -13,7 +13,6 @@ const Login = () => {
   useEffect(() => {
     const localToken = localStorage.getItem("token");
     if (localToken) {
-      // Periksa role pengguna yang sudah tersimpan
       const role = localStorage.getItem("role");
       if (role === "Admin") {
         navigate("/admin/home");
@@ -33,10 +32,9 @@ const Login = () => {
       const data = res.data;
       if (data.token) {
         localStorage.setItem("token", data.token);
-        localStorage.setItem("role", data.user_type); // Simpan 'user_type' sebagai 'role'
+        localStorage.setItem("role", data.user_type);
         toast.success(`Login successful! Welcome, ${data.user_type}`);
 
-        // Navigasi berdasarkan tipe pengguna
         if (data.user_type === "Admin") {
           navigate("/admin/home");
         } else if (data.user_type === "Operator") {
@@ -54,55 +52,68 @@ const Login = () => {
   };
 
   return (
-    <div className="w-full min-h-screen flex items-start">
-      <div className="relative w-1/2 h-full flex flex-col">
-        <div className="absolute top-[30%] left-[15%] flex flex-col"></div>
-        <img
-          src={COVER_IMAGE}
-          alt="Turret Gun"
-          className="w-full h-full object-cover"
-        />
-      </div>
+    <div className="flex justify-center items-center min-h-screen bg-slate-900 ">
+      <form
+        className="max-w-md mx-auto bg-[#273444] bg-opacity-100 p-8 rounded-3xl shadow-lg w-2/5"
+        onSubmit={handleSubmit}
+      >
+        <h2 className="text-2xl font-bold mb-2 text-left dark:text-white">
+          Login
+        </h2>
+        <h3 className="text-base mb-16 text-left dark:text-gray-300">
+          Sign in to your account to continue
+        </h3>
 
-      <div className="w-1/2 h-screen bg-[#F5F5F5] flex flex-col p-20 gap-28">
-        <h1 className="text-base text-[#060606] font-semibold">AviSent</h1>
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="text"
+            name="floating_name"
+            id="floating_name"
+            className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            value={values.name}
+            onChange={(e) => setValues({ ...values, name: e.target.value })}
+            required
+          />
+          <label
+            htmlFor="floating_name"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Name
+          </label>
+        </div>
 
-        <form
-          onSubmit={handleSubmit}
-          className="w-full flex flex-col max-w-[550px]"
-        >
-          <div className="w-full flex flex-col mb-2">
-            <h3 className="text-3xl font-bold mb-2">Login</h3>
-          </div>
-          <div className="w-full flex flex-col">
-            <input
-              type="text"
-              placeholder="Name"
-              className="w-full text-black py-4 my-2 bg-transparent border-b border-black outline-none focus:outline-none"
-              value={values.name}
-              onChange={(e) => setValues({ ...values, name: e.target.value })}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              className="w-full text-black py-4 my-2 bg-transparent border-b border-black outline-none focus:outline-none"
-              value={values.password}
-              onChange={(e) =>
-                setValues({ ...values, password: e.target.value })
-              }
-            />
-          </div>
-          {error && <div className="text-red-500 text-sm my-2">{error}</div>}
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="password"
+            name="floating_password"
+            id="floating_password"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            value={values.password}
+            onChange={(e) => setValues({ ...values, password: e.target.value })}
+            required
+          />
+          <label
+            htmlFor="floating_password"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Password
+          </label>
+        </div>
 
+        {error && <div className="text-red-500 text-sm my-2">{error}</div>}
+
+        <div className="text-center mt-10">
           <button
             type="submit"
-            className="flex my-4 text-white bg-[#697077] rounded-md p-4 text-center items-center justify-center hover:bg-[#46423c]"
+            className="text-white bg-[#697077] hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-[#f8dbb3] font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
           >
             Login
           </button>
-        </form>
+        </div>
         <ToastContainer />
-      </div>
+      </form>
     </div>
   );
 };
